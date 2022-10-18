87fbe665963e7800199fc0273cbd30cdf3f632c7
williampansky@gmail.com <williampansky@gmail.com>
2022-10-17 19:09:17 -0500

    extracted zone in-turn interactions to new subdir

--- src/phases/init-zone-interactions.phase.ts
+++ src/phases/init-zone-interactions.phase.ts
@@ -1,96 +1,11 @@
 import { Ctx, PhaseConfig } from 'boardgame.io';
-import { Card, GameState, Minion, Zone } from '../interfaces';
-import getCardPower from '../utilities/get-card-power';
+import { GameState } from '../interfaces';
+import { onCardPlayZoneInteractions } from '../zone-interactions';
 
 const initZoneInteractions: PhaseConfig = {
-  next: 'drawCard',
   onBegin(G: GameState, ctx: Ctx) {
     console.log(G.turn, ctx.phase);
-    const { turn, zones } = G;
-
-    // loop thru each zone
-    zones.forEach((z: Zone, zoneIdx: number) => {
-      if (z.revealed) {
-        switch (z.id) {
-          case 'ZONE_001':
-          case 'ZONE_004':
-          case 'ZONE_007':
-          case 'ZONE_008':
-            // loop thru side 0
-            z.sides['0'].forEach((obj: Minion, cardIdx: number) => {
-              // if (turn === obj.revealedOnTurn) {
-                G.zones[zoneIdx].sides['0'][cardIdx].zonePowerAdjustment =
-                  z.powerAdjustment;
-                G.zones[zoneIdx].sides['0'][cardIdx] = {
-                  ...obj,
-                  displayPower: getCardPower(obj),
-                };
-              // }
-            });
-
-            // loop thru side 1
-            z.sides['1'].forEach((obj: Minion, cardIdx: number) => {
-              // if (turn === obj.revealedOnTurn) {
-                G.zones[zoneIdx].sides['1'][cardIdx].zonePowerAdjustment =
-                  z.powerAdjustment;
-                G.zones[zoneIdx].sides['1'][cardIdx] = {
-                  ...obj,
-                  displayPower: getCardPower(obj),
-                };
-              // }
-            });
-            // zone.sides[playerId].forEach((c: Card, i: number) => {
-            //   G.zones[zoneNumber].sides[playerId][i] = {
-            //     ...c,
-            //     zonePowerAdjustment: zone.powerAdjustment,
-            //   };
-            //   // if (G.zones[zoneNumber].sides[playerId][i].powerStream) {
-            //   //   const stream = G.zones[zoneNumber].sides[playerId][i].powerStream!;
-            //   //   const streamLength = stream.length;
-            //   //   const lastStreamPower = stream[streamLength].currentPower;
-
-            //   //   G.zones[zoneNumber].sides[playerId][i].powerStream!.push({
-            //   //     blame: zone.id,
-            //   //     adjustment: zone.powerAdjustment,
-            //   //     currentPower: add(lastStreamPower, zone.powerAdjustment),
-            //   //   });
-            //   // } else {
-            //   //   G.zones[zoneNumber].sides[playerId][i].powerStream = [{
-            //   //     blame: zone.id,
-            //   //     adjustment: zone.powerAdjustment,
-            //   //     currentPower: add(c.basePower, zone.powerAdjustment),
-            //   //   }];
-            //   // }
-            // });
-            break;
-          case 'ZONE_010':
-            // loop thru side 0
-            z.sides['0'].forEach((obj: Minion, cardIdx: number) => {
-              // if (turn === obj.revealedOnTurn) {
-                G.zones[zoneIdx].sides['0'][cardIdx] = {
-                  ...obj,
-                  displayPower: obj.currentCost,
-                };
-              // }
-            });
-
-            // loop thru side 1
-            z.sides['1'].forEach((obj: Minion, cardIdx: number) => {
-              // if (turn === obj.revealedOnTurn) {
-                G.zones[zoneIdx].sides['1'][cardIdx] = {
-                  ...obj,
-                  displayPower: obj.currentCost,
-                };
-              // }
-            });
-            break;
-          default:
-            break;
-        }
-      }
-    });
-
-    // end the phase
+    onCardPlayZoneInteractions(G);
     ctx.events?.endPhase();
   },
 };
