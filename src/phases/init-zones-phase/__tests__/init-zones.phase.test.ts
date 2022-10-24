15606d98cc47692855dec447c2f4301d888bdbfa
williampansky@gmail.com <williampansky@gmail.com>
2022-10-24 09:36:23 -0500

    fixed up init-zones.phase.test

--- src/phases/init-zones-phase/__tests__/init-zones.phase.test.ts
+++ src/phases/init-zones-phase/__tests__/init-zones.phase.test.ts
@@ -3,9 +3,31 @@ import { mockCtx } from '../../../test-utils';
 import { initZonesPhase } from '..';
 
 describe('Handles state manipulation of G.Zones', () => {
-  test('Should create playable zone objects', () => {
+  test('Should create three unique zone objects during onBegin()', () => {
     let G = DefaultState;
     initZonesPhase.onBegin!(G, mockCtx());
+
     expect(G.Zones[0].uuid).not.toBeUndefined();
+    expect(G.Zones[0].uuid).not.toBe('');
+    expect(G.Zones[0].id).not.toBe(G.Zones[1].id);
+    expect(G.Zones[0].id).not.toBe(G.Zones[2].id);
+
+    expect(G.Zones[1].uuid).not.toBeUndefined();
+    expect(G.Zones[1].uuid).not.toBe('');
+    expect(G.Zones[1].id).not.toBe(G.Zones[0].id);
+    expect(G.Zones[1].id).not.toBe(G.Zones[2].id);
+
+    expect(G.Zones[2].uuid).not.toBeUndefined();
+    expect(G.Zones[2].uuid).not.toBe('');
+    expect(G.Zones[2].id).not.toBe(G.Zones[0].id);
+    expect(G.Zones[2].id).not.toBe(G.Zones[1].id);
+  });
+
+  test('Should get true from Zones.areReady during endIf()', () => {
+    let G = DefaultState;
+    initZonesPhase.onBegin!(G, mockCtx());
+    
+    const check = initZonesPhase.endIf!(G, mockCtx());
+    expect(check).toBe(true);
   });
 });
--- src/phases/init-zones-phase/init-zones.phase.ts
+++ src/phases/init-zones-phase/init-zones.phase.ts
@@ -8,24 +8,36 @@ const initZonesPhase: PhaseConfig = {
   start: true,
   onBegin(G: GameState, ctx: Ctx) {
     if (G.Config.debugConfig.logPhaseToConsole) console.clear();
+
     const { random } = ctx;
     const randomZonesArray = random?.Shuffle(ZONE_DATABASE);
+    const isCtxRandom = randomZonesArray?.forEach(z => z !== undefined);
     const withUuid = true;
     let newZones: Zone[] = [];
 
-    for (let idx = 0; idx < G.Config.gameConfig.numberOfZones; idx++) {
-      let newZone = createZoneObject(randomZonesArray![idx], withUuid);
-      newZones.push(newZone);
+
+    // check if ctx.random?.Shuffle is working
+    // usually seems to fail in mockCtx test util
+    if (isCtxRandom) {
+      for (let idx = 0; idx < G.Config.gameConfig.numberOfZones; idx++) {
+        let newZone = createZoneObject(randomZonesArray![idx], withUuid);
+        newZones.push(newZone);
+      }
+    } else {
+      const backupZonesArray = ZONE_DATABASE;
+      for (let idx = 0; idx < G.Config.gameConfig.numberOfZones; idx++) {
+        let newZone = createZoneObject(backupZonesArray![idx], withUuid);
+        newZones.push(newZone);
+      }
     }
 
     Zones.set(G, newZones);
 
     if (G.Config.debugConfig.logPhaseToConsole) {
-      console.log(
-        G.turn,
-        ctx.phase,
-        `| 0: ${G.Zones[0].name}, 1: ${G.Zones[1].name}, 2: ${G.Zones[2].name}`
-      );
+      const one = G.Zones[0].name;
+      const two = G.Zones[1].name;
+      const three = G.Zones[2].name;
+      console.log(G.turn, ctx.phase, `| 0: ${one}, 1: ${two}, 2: ${three}`);
     }
   },
   endIf: (G: GameState) => Zones.areReady(G),
